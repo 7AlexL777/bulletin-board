fa652ed1b5d2786d10e40663e31772de84c43746
alexL <litvinaualex@gmail.com>
2022-05-31 14:29:57 +0300

    add model Rubric

--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,28 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# from django.template import loader
+#
+# from bboard.models import Bb
+#
+#
+# def index(request):
+#     template = loader.get_template('bboard/index.html')
+#     bbs = Bb.objects.order_by('-published')
+#     context = {'bbs': bbs}
+#     return HttpResponse(template.render(context, request))
+
+from django.shortcuts import render
+from .models import Bb, Rubric
 
 
 def index(request):
-    return HttpResponse("Здесь будет выведен список объявлений.")
+    bbs = Bb.objects.all()
+    context = {'bbs': bbs}
+    return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
