47479e560e4ab79e5790ddb602841c082330e1ad
alexL <litvinaualex@gmail.com>
2022-06-01 12:06:08 +0300

    create BbForm to add ads

--- bboard/views.py
+++ bboard/views.py
@@ -11,12 +11,17 @@
 #     return HttpResponse(template.render(context, request))
 
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import BbForm
 from .models import Bb, Rubric
 
 
 def index(request):
     bbs = Bb.objects.all()
-    context = {'bbs': bbs}
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
     return render(request, 'bboard/index.html', context)
 
 
@@ -26,3 +31,16 @@ def by_rubric(request, rubric_id):
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+
